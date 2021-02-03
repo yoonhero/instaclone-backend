3f846d862f163282e6b5284dd0e17f5c43cce444
yoonhero <yoonhero06@naver.com>
2021-02-03 14:16:18 +0900

    #2/3

---
+++ users/users.mutations.js
@@ -0,0 +1,50 @@
+import bcrypt from 'bcrypt';
+import client from "../client"
+
+export default {
+    Mutation: {
+        craeteAccount: async (_, {
+            firstName,
+            lastName,
+            username,
+            email,
+            password,
+        }) => {
+            try {
+                // check if username or email are already on DB.
+                const existingUser = await client.user.findFirst({
+                    where: {
+                        OR: [
+                            {
+                                username,
+                            },
+                            {
+                                email,
+                            },
+                        ],
+                    },
+                });
+
+                if (existingUser) {
+                    throw new Error("This username/password is already taken.");
+                }
+
+                // hash password
+                const uglyPassword = await bcrypt.hash(password, 10);
+                return client.user.create({
+                    data: {
+                        username,
+                        email,
+                        firstName,
+                        lastName,
+                        password: uglyPassword,
+                    },
+                });
+                // 1234 -> 1234 DB isn't safe 
+                // save and return user
+            } catch (e) {
+                return e;
+            }
+        },
+    },
+};
