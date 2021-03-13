dc3da10764d66cb5d28a580e4c9af56075005583
yoonhero <yoonhero06@naver.com>
2021-03-13 09:27:17 +0900

    3/13

--- shared/shared.utils.js
+++ shared/shared.utils.js
@@ -10,7 +10,8 @@ AWS.config.update({
 export const uploadToS3 = async (file, userId, folderName) => {
   const { filename, createReadStream } = await file;
   const readStream = createReadStream();
-  const objectName = `${folderName}/${userId}-${Date.now()}-${filename}`;
+  const objectName = `${folderName}/${userId}-${filename}`;
+
   const { Location } = await new AWS.S3()
     .upload({
       Bucket: "yoonhero-instaclone-uploads",
@@ -19,5 +20,6 @@ export const uploadToS3 = async (file, userId, folderName) => {
       Body: readStream,
     })
     .promise();
+
   return Location;
 };
